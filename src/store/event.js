603b445e62e21cdb0ead6d193d0aac22ca732c5f
Brooke Heck <b19heck@gmail.com>
2022-11-18 22:32:15 +0000

    resolve conflicts

--- src/store/event.js
+++ src/store/event.js
@@ -1,7 +1,23 @@
-const initialState = {}
+const initialState = {
+  user_id: null,
+  userEvents: [],
+}
+
+
 
 const reducer = (state=initialState, action) => {
+  switch(action.type) {
+    case 'get_all_event':
+      state = {...state, userEvents: action.payload};
+      return state;
+    case 'create_event': 
+      state = {...state, userEvents: [...state.userEvents, action.payload]};
+      return state;
+    case 'update_event':
+      state = {...state, }
 
+    default: return state;
+  }
 }
 
 export default reducer;
