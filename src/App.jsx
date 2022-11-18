26e7049413a15c0ee9afdaf17339780c5c23b07a
Alfredo <fredy_rios0123@outlook.com>
2022-11-18 19:50:33 +0000

    Basic login/sign in page

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+'use strict'
+
+import React from 'react';
+import Header from './Header'
+import Footer from './Footer';
+import Login from './Login/index.jsx';
+import './App.scss';
+
+const App = () => {
+
+    return (
+  <>
+    <Header />
+    <Login />
+    <Footer />
+  </>    
+  );
+};
+
+export default App;
