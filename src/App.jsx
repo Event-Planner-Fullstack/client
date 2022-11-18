45857c76764a8c027baac929318fb03d0f3e4984
Carlos A. Herrera <cherrera1208@gmail.com>
2022-11-18 18:37:19 +0000

    git ignore env

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+'use strict'
+
+import React from 'react';
+import Header from './Header'
+import Footer from './Footer';
+import Login from './Login';
+
+
+const App = () => {
+
+  return (
+    <>
+      <Header />
+      <Login />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
