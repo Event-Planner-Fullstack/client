360c9d789673a3b3f0e6a7cc11c8fed8dfb37734
Alfredo <fredy_rios0123@outlook.com>
2022-11-18 20:32:48 +0000

    Login page

--- src/Login/index.jsx
+++ src/Login/index.jsx
@@ -1,11 +1,27 @@
+'use strict';
+
+import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { Card, ListGroup } from 'react-bootstrap/esm';
 
 const Login = () => {
 
   return (
-    <>
-      <Button variant="primary">login</Button>{' '}
-    </>
+    <div>
+      <Card className="text-center">
+        <Card.Body>
+          <Card.Title>Web title</Card.Title>
+          <Card.Text>
+            Some quick text here.
+          </Card.Text>
+          <ListGroup.Item>
+            <Button variant="outline-primary">Log in</Button>
+            <Button variant="outline-info">Sign in</Button>
+          </ListGroup.Item>
+          <Button variant="outline-success">RSVP?</Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
