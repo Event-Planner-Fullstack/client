71f31d7d7a526c39f91129e7a2d4b11eb06938d7
Brooke Heck <b19heck@gmail.com>
2022-11-18 23:40:45 +0000

    add bootstrap css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
