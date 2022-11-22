e86ccb1ad44bb182740842f8e5ba856dc8a382a2
Brooke Heck <b19heck@gmail.com>
2022-11-22 23:06:47 +0000

    rm unused files

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import UserForm from '../../Forms/User';
-import RSVP from '../../Forms/RSVP';
+import LoginModal from '../../Forms/LoginModal';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Brand from '../../Brand';
 import CarouselPage from '../../Carousel';
-import Calendar from '../Calendar';
 import './Login.scss';
 
 const Login = () => {
@@ -17,16 +15,11 @@ const Login = () => {
         <Row className="page">
           <Col className="brand">
             <Brand/>
-            {/* <Calendar/> */}
             <CarouselPage className="carousel"/>
           </Col>
           <Col xs={4} className="login">
-            <UserForm />
-            
+            <LoginModal />
           </Col>
-        </Row>
-        <Row>
-          
         </Row>
       </Container>
     </>
