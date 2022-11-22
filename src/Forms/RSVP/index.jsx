875225be5ec5090d19c6c25889c1e21b0d66fdb6
Brooke Heck <b19heck@gmail.com>
2022-11-22 14:41:58 +0000

    guest list working

--- src/Forms/RSVP/index.jsx
+++ src/Forms/RSVP/index.jsx
@@ -1,34 +1,33 @@
-import React, { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import GuestForm from './GuestForm';
 
 function RSVP() {
-  const [showModal, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const dispatch = useDispatch();
+
+  const showModal = useSelector(state => state.modals.guest_modal);
+
+  const toggleModal = () => {
+    dispatch({type: 'guest_modal'});
+  }
+
   return (
     <>
       <Container className="d-grid gap-2">
-        <Button variant="success" size="lg" onClick={handleShow}>
-          RSVP
-        </Button>
-        <Modal show={showModal} onHide={handleClose}>
+        
+        <Button variant="success" size="lg" onClick={toggleModal}> RSVP </Button>
+        
+        <Modal show={showModal} onHide={toggleModal}>
           <Modal.Header closeButton>
-            <Modal.Title>Guest Modal</Modal.Title>
+            <Modal.Title>RSVP to an Event</Modal.Title>
           </Modal.Header>
-          <Modal.Body>
-            test
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
+          
+          <Modal.Body><GuestForm/></Modal.Body>
+
         </Modal>
+
       </Container>
     </>
   );
