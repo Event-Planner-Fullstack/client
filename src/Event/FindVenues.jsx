8b449816538d6433d4706f0c4c80eaa1156d6974
Brooke Heck <b19heck@gmail.com>
2022-11-22 18:01:42 +0000

    refresh button working

--- src/Event/FindVenues.jsx
+++ src/Event/FindVenues.jsx
@@ -1,18 +1,10 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import readAll from './../store/middleware/crud/readAll';
+import { useSelector } from 'react-redux';
 import VenueCard from './VenueCard';
 
 const FindVenues = () => {
-  const dispatch = useDispatch();
-
-  const user = useSelector(state => state.user.user);
   const venueList = useSelector(state => state.venue.userVenues);
 
-  useEffect(() => {
-    dispatch(readAll(user.token, 'venue'));
-    dispatch(readAll(user.token, `event/user/${user.id}`));
-  }, []);
+
 
   return (
     <>
